a13a6df3c3cf85ae130e14da6d2d7ed08829d5d4
Mia <mia.chen@berkeley.edu>
2017-06-18 23:47:09 +0200

    add things

--- angel_co_scrapping.py
+++ angel_co_scrapping.py
@@ -16,12 +16,14 @@ import sys
 #PATH = '/usr/local/bin/chromedriver'
 PATH = 'C:/Users/vtec-mchen/PycharmProjects/chromedriver.exe'
 Login = ["myouhu@yahoo.fr", "bismilah"]
-research = ["Paris"]
+research = ["Paris", ""]
 
 ######################################################
 
 ##################Variable############################
 List_of_docs = []
+number_startups = 0
+nb_page_scrapped = 0
 
 ##################Fonction###########################
 def Convert_to_csv(List_of_docs):
@@ -32,8 +34,41 @@ def Convert_to_csv(List_of_docs):
         dict_writer.writeheader()
         dict_writer.writerows(List_of_docs)
 
+def scrap_website(List_of_docs,number_startups):
+
+    tagline = driver.find_elements_by_css_selector("div.tagline")  # find tagline
+    jobs_title = driver.find_elements_by_css_selector("div.collapsed-listing-row")  # find number of jobs
+    startup_names = driver.find_elements_by_css_selector("a.startup-link")  # find startup's name
+    print("display nb of startups scrapped")
+    print(len(tagline), len(jobs_title), len(startup_names))
+
+    number_startups = number_startups + len(startup_names)
+
+    for index, name in enumerate(startup_names):
+        dictionnaire = {}
+        try:
+            # print(name.text)
+            dictionnaire["id"] = index
+            dictionnaire["name"] = name.text
+
+        except:
+            print("saute")
+        List_of_docs.append(dictionnaire)
+
+    for index, name in enumerate(tagline):
+        List_of_docs[index]['tagline'] = name.text.encode("utf-8")
+
+    for index, name in enumerate(jobs_title):
+        # List_of_docs[index]['jobs'] = name.text.encode("utf-8")
+        print(index, name.text.encode("utf-8"))
+
+    return List_of_docs,number_startups
 
 
+
+
+
+##################Login#############################
 driver = webdriver.Chrome(PATH)
 driver.get('http://angel.co/job')
 
@@ -54,57 +89,38 @@ pw.send_keys(Login[1])
 driver.implicitly_wait(5) # seconds
 login = driver.find_element_by_name("commit")
 login.click()
+
+#################Enter words in serach bar###############
+
 input = driver.find_element_by_css_selector("div.search-box")
 input.click()
 driver.implicitly_wait(14000)
 keyword= driver.find_element_by_xpath("//input[@class='input keyword-input']")
-
-
-
 
 for i in research:
     keyword.send_keys(i)
     driver.implicitly_wait(5)  # seconds
 
 button_save = driver.find_elements_by_css_selector("a.save-current-filters.c-button.c-button--gray.c-button--sm")[0]
-button_save.click()
+button_save.click() #wrong button
 
-nb_startups = driver.find_elements_by_css_selector("span.label")[-1].text
+################### Scrap pages #########################
 
+nb_startups = driver.find_elements_by_css_selector("span.label")[-1].text #find total number of startups
 
-tagline = driver.find_elements_by_css_selector("div.tagline")
-jobs_title = driver.find_elements_by_css_selector("div.collapsed-listing-row")
-startup_names = driver.find_elements_by_css_selector("a.startup-link")
-#info_table = driver.find_elements_by_css_selector("div.startup-info-table")
+while number_startups != nb_startups:
+    print("before %s " %(number_startups))
+    List_of_docs,number_startups = scrap_website(List_of_docs,number_startups)
+    print("after %s" %number_startups)
+    # scroll down the page
+    driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+    print(List_of_docs)
+    nb_page_scrapped +=1
 
-print(len(tagline), len(jobs_title), len(startup_names))
-
-for index, name in enumerate(startup_names):
-    dictionnaire = {}
-    try:
-        #print(name.text)
-        dictionnaire["id"] = index
-        dictionnaire["name"] = name.text
-
-    except:
-        print("saute")
-    List_of_docs.append(dictionnaire)
+print(number_startups, nb_page_scrapped)
+Convert_to_csv(List_of_docs)
 
 
-for index, name in enumerate(tagline):
-        List_of_docs[index]['tagline'] = name.text.encode("utf-8")
-
-for index, name in enumerate(jobs_title):
-        #List_of_docs[index]['jobs'] = name.text.encode("utf-8")
-        print(index, name.text.encode("utf-8"))
-
-
-
-print(List_of_docs)
-#Convert_to_csv(List_of_docs)
-
-#scroll down the page
-driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
 
 
 
